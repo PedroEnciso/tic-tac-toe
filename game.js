e0fc02df7ca22d8a8d416c71d1dfe3abaf1ef04a
Pedro Enciso <penciso@ucsd.edu>
2021-05-30 11:15:25 -0700

    fill spaces, check for winners

--- game.js
+++ game.js
@@ -1,9 +1,87 @@
 const GameBoard = (() => {
   // get DOM elements
-  const boardSpaces = document.querySelectorAll(".space");
+  const gameBoard = document.querySelectorAll(".space");
 
-  // initialize gameboard array
-  const gameBoard = new Array(9);
+  let whoseTurn = true;
 
-  console.log(boardSpaces[0]);
+  for (let i = 0; i < gameBoard.length; i++) {
+    gameBoard[i].addEventListener("click", () => playerTurn(gameBoard[i]));
+  }
+
+  // functions
+  const playerTurn = (space) => {
+    whoseTurn ? (space.innerHTML = "X") : (space.innerHTML = "O");
+    whoseTurn = !whoseTurn;
+    let winner = checkForWinner(gameBoard);
+    if (winner === "X") alert("Player 1 wins!");
+    else if (winner === "O") alert("Player 2 wins!");
+  };
+
+  const checkForWinner = (gameBoard) => {
+    if (checkRows()) return checkRows();
+    if (checkCols()) return checkCols();
+    if (checkDiagonals()) return checkDiagonals();
+  };
+
+  const checkRows = () => {
+    if (
+      gameBoard[0].innerHTML === gameBoard[1].innerHTML &&
+      gameBoard[1].innerHTML === gameBoard[2].innerHTML
+    ) {
+      return gameBoard[0].innerHTML;
+    }
+
+    if (
+      gameBoard[3].innerHTML === gameBoard[4].innerHTML &&
+      gameBoard[4].innerHTML === gameBoard[5].innerHTML
+    ) {
+      return gameBoard[3].innerHTML;
+    }
+
+    if (
+      gameBoard[6].innerHTML === gameBoard[7].innerHTML &&
+      gameBoard[7].innerHTML === gameBoard[8].innerHTML
+    ) {
+      return gameBoard[6].innerHTML;
+    }
+  };
+
+  const checkCols = () => {
+    if (
+      gameBoard[0].innerHTML === gameBoard[3].innerHTML &&
+      gameBoard[3].innerHTML === gameBoard[6].innerHTML
+    ) {
+      return gameBoard[0].innerHTML;
+    }
+
+    if (
+      gameBoard[1].innerHTML === gameBoard[4].innerHTML &&
+      gameBoard[4].innerHTML === gameBoard[7].innerHTML
+    ) {
+      return gameBoard[1].innerHTML;
+    }
+
+    if (
+      gameBoard[2].innerHTML === gameBoard[5].innerHTML &&
+      gameBoard[5].innerHTML === gameBoard[8].innerHTML
+    ) {
+      return gameBoard[2].innerHTML;
+    }
+  };
+
+  const checkDiagonals = () => {
+    if (
+      gameBoard[0].innerHTML === gameBoard[4].innerHTML &&
+      gameBoard[4].innerHTML === gameBoard[8].innerHTML
+    ) {
+      return gameBoard[0].innerHTML;
+    }
+
+    if (
+      gameBoard[1].innerHTML === gameBoard[4].innerHTML &&
+      gameBoard[4].innerHTML === gameBoard[7].innerHTML
+    ) {
+      return gameBoard[1].innerHTML;
+    }
+  };
 })();
